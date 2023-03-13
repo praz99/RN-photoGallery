8df2c684aa193ada7c9c97b4cda88278dcf79d27
Praz <t.prazwal@gmail.com>
2023-03-13 04:46:46 -0600

    Modify App.jsx and add api/picsum.jsx

---
+++ api/picsum.jsx
@@ -0,0 +1,15 @@
+const BASE_URL = 'https://picsum.photos/v2';
+
+const getList = async (page = 1) => {
+  const response = await fetch(`${BASE_URL}/list?page=${page}`);
+  const photos = await response.json();
+  return photos;
+};
+
+const formatPhotoUri = (id, width, height) => {
+  return `https://picsum.photos/id/${id}/${Math.floor(width)}/${Math.floor(
+    height,
+  )}`;
+};
+
+export {getList, formatPhotoUri};
