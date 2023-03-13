99f811f2d2706510fb073ea7e2b65e9cdd8b97fc
Praz <t.prazwal@gmail.com>
2023-03-13 04:58:40 -0600

    add initial state and reducer function

--- reducers/photos.jsx
+++ reducers/photos.jsx
@@ -13,4 +13,30 @@ const actionCreators = {
   }),
 };
 
-export {actionCreators};
+const initialState = {
+  loading: false,
+  error: false,
+  photos: [],
+  nextPage: 1,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case types.LOADING:
+      return {...state, loading: true, error: false};
+
+    case types.SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        photos: [...state.photos, action.payload.photos],
+        nextPage: state.nextPage + 1,
+      };
+
+    case types.FAILURE:
+      return {...state, loading: false, error: true};
+  }
+};
+
+export {actionCreators, initialState};
